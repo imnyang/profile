592d1222002c0dbb831e850ac01cd8686598d5a0
imnyang <nyang.nyanglab@gmail.com>
2023-11-08 19:09:40 +0900

    미쿠 미쿠 미쿠 요이루

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import Profile from "@/../public/profile.jpg";
 export default function Home() {
   return (
     <main>
-      <header></header>
-      <div className="DivImage">
-        <Image alt="Profile" src={Profile} width={80} height={80} />
-      </div>
+      <header>
+        <div className="DivImage">
+          <Image alt="Profile" src={Profile} width={85} height={85} />
+        </div>
+      </header>
     </main>
   );
 }
