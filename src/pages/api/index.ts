66d522884b365a379df147fc2501faf9faff104c
imnyang <me@imnyang.xyz>
2023-11-08 21:31:38 +0900

    끝

---
+++ src/pages/api/index.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(400).json({ message: '여기서 뭐하는거야~ 헨타이' })
+}
