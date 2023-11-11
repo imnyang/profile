a2136c9e78142c26031c796ff4f6f58b5fe1ae6c
imnyang <me@imnyang.xyz>
2023-11-11 21:23:23 +0900

    이런

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head lang="ko">
+            <title>imnyang</title>
+            <meta name="description" content="Made with 💕" />
+        </Head>
       <body>
         <Main />
         <NextScript />
