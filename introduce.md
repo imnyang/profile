7ea2e5e32dca7874bd1e07571308603466503baa
imnyang <nyang.nyanglab@gmail.com>
2023-11-11 18:10:22 +0900

    시발

---
+++ introduce.md
@@ -0,0 +1,10 @@
+<!--
+    Markdown 문법이 사용 가능하지만 제목같은 글자 강조는 사용이 불가능하고
+    일부 기기에서 길면 잘릴수도 있습니다. 6줄 정도가 딱 적당합니다.
+-->
+Blog : blog.imnyang.xyz
+
+
+Dis : sqlr.kr/imnyang
+
+Mail : me@imnyang.xyz
---
+++ profile.config.ts
@@ -0,0 +1,6 @@
+export const setting = {
+  display_name: "아임냥 💕", // 표시될 사용자 이름
+  name: "imnyang", // 디스코드 사용자 이름으로 복사될 이름
+  pronouns: "Sqlare와 비행 중", // 대명사 및 상태로 사용
+  profile_image: "profile.jpg", //public 폴더 안에 넣은 파일의 이름을 적어주세요.
+};
